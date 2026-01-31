054be51a669d77117cb1d88a7ba55e33d496be47
MedhanshUrs <160248443+MedhanshUrs@users.noreply.github.com>
2026-01-31 22:20:02 +0530

    Update gpio_driver.c

--- Task4/Submission/lib/gpio_driver.c
+++ Task4/Submission/lib/gpio_driver.c
@@ -1,21 +1,13 @@
 #include "gpio_driver.h"
 #include <ch32v00x.h>
 
-/*
- * External LED
- * Port : GPIOD
- * Pin  : PD2
- * Logic: Active HIGH
- */
-
 #define LED_PORT   GPIOD
 #define LED_PIN    GPIO_Pin_2
 
 void gpio_led_init(void)
 {
     GPIO_InitTypeDef GPIO_InitStructure;
-
-    /* Enable clock for GPIOD */
+    
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD, ENABLE);
 
     GPIO_InitStructure.GPIO_Pin   = LED_PIN;
@@ -23,8 +15,6 @@ void gpio_led_init(void)
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 
     GPIO_Init(LED_PORT, &GPIO_InitStructure);
-
-    /* Start with LED OFF */
     GPIO_ResetBits(LED_PORT, LED_PIN);
 }
 
@@ -45,3 +35,4 @@ void gpio_led_toggle(void)
     else
         GPIO_SetBits(LED_PORT, LED_PIN);
 }
+
